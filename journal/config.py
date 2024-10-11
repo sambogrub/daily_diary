83624b44138e3d671256183ad22695bb72aba533
slacker-by-design <184398938+slacker-by-design@users.noreply.github.com>
2024-10-11 23:47:15 +0200

    Clean-up logging configuration and simplifiy logger usage
    * Adding all relevant logging settings to config module
    * Using LOGGING_ prefix for all logging settings
    * Simplifying the logging.Logger configuration
    (configure_logger function)
    * Simplifying logger usage in repository module
    * Adding logger initialization to main module

--- journal/config.py
+++ journal/config.py
@@ -1,8 +1,11 @@
+# DB section
 DATABASE_NAME = 'journal_data.db'
 ENTRIES_TABLE = 'entries'
 GOALS_TABLE = 'goals'
 GOALS_STATE_TABLE = 'goals_state'
-LOG_FILE = 'journal_app_logs.log'
 
-# debugging value - True turns on debugging messages
-DEBUG = True
+# logging section
+LOGGING_FILE_NAME = '../logs/journal_app.log'
+LOGGING_MAX_LOG_SIZE = 5 * 1024 * 1024
+LOGGING_FILE_BACKUP_COUNT = 5
+LOGGING_LEVEL = 'DEBUG'
