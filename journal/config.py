409395476a61a79fae3bbff8a2830588b1c6d2ac
sambogrub <137734917+sambogrub@users.noreply.github.com>
2024-10-11 20:39:53 -0500

    Changed LOGGING_FILE_NAME
    Moved journal_app.log to root/logs

--- journal/config.py
+++ journal/config.py
@@ -5,7 +5,7 @@ GOALS_TABLE = 'goals'
 GOALS_STATE_TABLE = 'goals_state'
 
 # logging section
-LOGGING_FILE_NAME = '../logs/journal_app.log'
+LOGGING_FILE_NAME = 'logs/journal_app.log'
 LOGGING_MAX_LOG_SIZE = 5 * 1024 * 1024
 LOGGING_FILE_BACKUP_COUNT = 5
 LOGGING_LEVEL = 'DEBUG'
