451f7f79ddfbc0d42131f7ebd3d2de2a3c987e31
slacker-by-design <184398938+slacker-by-design@users.noreply.github.com>
2024-10-17 14:30:59 +0200

    Review of repository class structure
    * JournalData is not a repository. It doesn't work directly with DB
    tables and it contains "business logic". This makes it a controller.
    Therefore I moved it to controller module.
    * DB initialization is not a typical repository responsibility. It's
    better to isolate this functionality to a dedicated function (or class).
    * Repositories don't typically establish their own connection to a DB.
    It's given to them by a 3rd party. You're not dealing with multiuser,
    highly parallel system here, hence a single connection passed to the
    repository upon its construction should suffice.

--- journal/controller.py
+++ journal/controller.py
@@ -1,10 +1,69 @@
 # main journal controller. will interact with both the ui and data access
+"""
+TODO: Add module docs...
+"""
 
-from model import Month
-from repository import JournalData
-from datetime import datetime, date
+import calendar
+import datetime
+
 from dateutil.relativedelta import relativedelta
+
 from gui import JournalGUI
+from model import Month
+from repository import EntriesRepository, GoalsRepository
+
+
+class JournalData():
+    def __init__(self):
+        self.entry_repo = EntriesRepository()
+        self.goals_repo = GoalsRepository()
+
+    # get all the data needed for each day in the month. takes a date, and returns a list of dictionaries for the whole month
+    # returns [{date: {entry: entry,goal_id:(description,state)}}]
+    def populate_month_data(self, date: datetime.date) -> list[dict]:
+        # get the current year and month from the given date then get the first and last days of the current month
+        year = date.year
+        month = date.month
+        first_day = self.format_date_for_repos(datetime.date(year, month, 1))
+        last_day_cal = calendar.monthrange(year, month)
+        last_day = self.format_date_for_repos(datetime.date(year, month, last_day_cal[1]))
+
+        # get the journal entries for the month
+        entries = self.entry_repo.get_monthly_entries(first_day, last_day)
+        goals = self.goals_repo.get_goals()
+        goal_states = self.goals_repo.get_monthly_states(first_day, last_day)
+
+        goal_states_and_descriptions = []
+
+        # add the descriptions to the tuples of id and state
+        for goal_state in goal_states:
+            goal_id = goal_state[0]
+            state = goal_state[1]
+            for goal in goals:
+                if goal[0] == goal_id:
+                    goal_states_and_descriptions.append((goal_id, goal[1], state))
+
+        return entries, goal_states_and_descriptions
+
+    # ensure that the dates passed to the repo are in the correct string format
+    def format_date_for_repos(self, date) -> str:
+        formatted_date = date.isoformat()
+        return formatted_date
+
+    def check_if_entry(self, date, entry: str, goals: list[dict] = None):
+        if self.entry_repo.get_entry(date) == None:
+            self.save_day_data(date, entry, goals)
+        else:
+            self.update_day_data(date, entry, goals)
+
+    # saves new day data to tables
+    def save_day_data(self, date, entry: str, goals: list[dict] = None):
+        self.entry_repo.add_entry(date, entry)
+
+    # update entries data to tables
+    def update_day_data(self, date, entry: str, goals: list[dict] = None):
+        self.entry_repo.edit_entry(date, entry)
+
 
 class JournalController():
     def __init__(self, root):
@@ -15,7 +74,7 @@ class JournalController():
         self.ui = JournalGUI(root, self)
         
     def initialize_date(self):
-        today = date.today()
+        today = datetime.date.today()
         return today
 
     def get_month(self):
@@ -37,7 +96,6 @@ class JournalController():
 
     def decrease_day(self):
         self.current_date = self.current_date+relativedelta(days =- 1)
-    
 
     def add_day_entry(self,entry):
         day_num = self.current_date.day
@@ -49,6 +107,4 @@ class JournalController():
         day_num = self.current_date.day
         day = self.month.get_day(day_num)
         return day.entry  
-
-
 
